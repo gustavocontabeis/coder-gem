2ce2c99e5a4e88501d95e22b8d554ab7d2d8599d
Gustavo da Silva <gustavocontabeis@gmail.com>
2019-12-20 19:53:08 -0200

    OK

--- src/main/java/br/com/codersistemas/gem/components/Component.java
+++ src/main/java/br/com/codersistemas/gem/components/Component.java
@@ -2,7 +2,7 @@ package br.com.codersistemas.gem.components;
 
 import java.util.List;
 
-public abstract class Component implements IComponent{
+public abstract class Component implements IComponent {
 	
 	protected List<Component> components;
 
