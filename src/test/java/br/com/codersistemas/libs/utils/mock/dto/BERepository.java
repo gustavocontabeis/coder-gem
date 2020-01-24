8f94f92cdf8de647e630eb858e32cf54766df962
Gustavo da Silva <gustavocontabeis@gmail.com>
2020-01-24 18:04:34 -0200

    ok

--- src/test/java/br/com/codersistemas/libs/utils/mock/dto/BERepository.java
+++ src/test/java/br/com/codersistemas/libs/utils/mock/dto/BERepository.java
@@ -9,10 +9,10 @@ public class BERepository extends Resource {
 
 	public BERepository(EntidadeDTO entidade) {
 		this.entidade = entidade;
-		replace("Contratos", StringUtil.caplitalizePlural(entidade.getNomeClasse()));
-		replace("contratos", StringUtil.uncaplitalizePlural(entidade.getNomeClasse()));
-		replace("Contrato", StringUtil.caplitalizeSingular(entidade.getNomeClasse()));
-		replace("contrato", StringUtil.uncaplitalizeSingular(entidade.getNomeClasse()));
+//		replace("Contratos", StringUtil.caplitalizePlural(entidade.getNomeClasse()));
+//		replace("contratos", StringUtil.uncaplitalizePlural(entidade.getNomeClasse()));
+//		replace("Contrato", StringUtil.caplitalizeSingular(entidade.getNomeClasse()));
+//		replace("contrato", StringUtil.uncaplitalizeSingular(entidade.getNomeClasse()));
 	}
 
 	@Override
